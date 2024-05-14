11038d39d2937b3928e92e62186ad37ffc5c83e5
joecotten <joe.cotten95@gmail.com>
2024-05-13 19:24:47 -0500

    commit

--- pybank/main.py
+++ pybank/main.py
@@ -1,51 +1,50 @@
 import os
 import csv
-from pathlib import Path
 
-file = os.path.join("pybank","resources","budget_data.csv")
+data = csv.reader(open('Resources/budget_data.csv'))
+my_report = open('Analysis/budget_analysis.text','w')
 
-total_months = []
-total_profit = []
-monthly_change = []
+months = 0
+total = 0
+total_ch = 0
+prev_rev = 0
+inc = ["", 0]
+dec = ["", 0]
+header = next(data)
 
-with open(file) as filepath:
-    csvreader = csv.reader(filepath, delimiter=",")
+for r in data:
+   months += 1 #append; months = months +1
 
-    header = next(csvreader)
+   rev = int(r[1])
+   total += rev
 
-    for row in csvreader:
-        total_months.append(row[0])
-        total_profit.append(int(row[1]))
+   ch = rev - prev_rev
+   if prev_rev == 0:
+      ch = 0 
 
-    for i in range(len(total_profit)-1):
+   total_ch += ch #change
 
-        monthly_change.append(total_profit[i+1]-total_profit[i])
+   #greatest increase 
+   if ch > inc[1]:
+      inc[0] = r[0]
+      inc[1] = ch
+   
+   #greatest decrease
+   if ch < dec[1]:
+      dec[0] = r[0]
+      dec[1] = ch
 
-    max_increase = max(monthly_change)
-    max_decrease = min(monthly_change)
+   prev_rev = rev #reset variable
 
-    print("Financial Analysis")
-    print ("----------------------------")
-    print (f"Total Months{len(total_months)}")
-    print (f"Total: ${sum(total_profit)}")
-    #print (f"Average Change: ${round(sum(monthly_change)/ len(monthly_change),2)}")
-    #print (f"Greatest Increase in Profits: {total_months[max_increase]}(${(str(max_increase))})")
-    #print (f"Greatest Decrease in Profits: {total_months[max_decrease]}(${(str(max_decrease))})")
+output = f'''
+Financial Analysis
+----------------------------
+Total Months: {months}
+Total: ${total:,}
+Average Change: ${total_ch/(months-1):,.2f}
+Greatest Increase in Profits: {inc[0]} (${inc[1]:,})
+Greatest Decrease in Profits: {dec[0]} (${dec[1]:,})
+'''
 
-    output = os.path.join("pybank","analysis", "output.csv")
-
-    with open(output, "w") as file:
-
-        file.write("Financial Analysis")
-        file.write("\n")
-        file.write("----------------------------")
-        file.write("\n")
-        file.write(f"Total Months: {len(total_months)}")
-        file.write("\n")
-        file.write(f"Total: ${sum(total_profit)}")
-        file.write("\n")
-        file.write(f"Average Change: {round(sum(monthly_change)/ len(monthly_change),2)}")
-        file.write("\n")
-        file.write(f"Greatest Increase in Profits: {total_months[max_increase]}(${(str(max_increase))})")
-        file.write("\n")
-        file.write(f"Greatest Decrease in Profits: {total_months[max_decrease]}(${(str(max_decrease))})")
+print(output)
+my_report.write(output)
